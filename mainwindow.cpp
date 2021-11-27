bcc5ed2413e1511542f2120c81cbc476d71738f1
Muhammad Ali Tanveer <tanveer7652@gmail.com>
2021-11-27 20:22:00 +0500

    Funcbreaker completed

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QStack>
 using namespace std;
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -14,23 +15,48 @@ MainWindow::~MainWindow()
 }
 vector<QString> MainWindow::funcbreaker()
 {
+    QStack<QChar>bracket;
     vector<QString> partfuncs;
     QString input = ui->Chp4FuncInput->text();
     //ui->Chp4formulalabel->setText(input);
     QChar *arr = new QChar[input.length()];
     int i, j,k;
+    bool flag=false;
     for(i=0,j=0;i<input.length();i++,j++)
     {
-        arr[j]=input[i];
+        if(input[i]=='(')
+        {
+            flag=true;
+            bracket.push('(');
+        }
+        if(input[i]==')')
+        {
+            bracket.pop();
+            if(bracket.empty())
+            flag=false;
+        }
         if(input[i]==' ')
+        {
+            j--;
             continue;
-        if(input[i]=='+'||input[i]=='*'||input[i]=='-'||input[i]=='/')
+        }
+        if(input[i]=='='&&flag==false)
+        {
+            arr[j] = '\0';
+            QString temp(arr);
+            partfuncs.push_back(temp);
+            j=-1;
+            break;
+        }
+        arr[j]=input[i];
+        if((input[i]=='+'||input[i]=='*'||input[i]=='-'||input[i]=='/')&&flag==false)
         {
             arr[j] = '\0';
             QString temp(arr);
             partfuncs.push_back(temp);
             j=-1;
         }
+
     }
 
     return partfuncs;
